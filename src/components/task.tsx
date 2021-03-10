a7d2a860763783604485cb11d37b128a28d2323e
Nam Bui <nambui@Nams-MacBook-Air.local>
2021-03-09 19:22:50 -0800

    Add task component

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { 
+    Text, 
+    View, 
+    TouchableOpacity, 
+    StyleSheet,
+} from 'react-native';
 
-const Task = () => {
+const Task = ( props: { task: String } ) => {
     return (
-        <Text>
-            This is a task
-
-        </Text>
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Text style={styles.text}>
+                    This is a task
+                </Text>
+            </TouchableOpacity>
+        </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      // flex: 1,
+      height: 80,
+      borderBottomColor: '#bbb',
+      borderStyle: 'solid',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      //backgroundColor: 'red',
+    },
+    text: {
+      flex: 5,
+      fontWeight: '400',
+      fontSize: 18,
+      marginVertical: 30,
+      marginHorizontal: 30,
+      width: 'auto',
+      alignSelf: 'center',
+    },
+    circle: {
+      width: 30,
+      height: 30,
+      borderRadius: 15,
+      borderColor: 'blue',
+      borderWidth: 2,
+      marginRight: 20,
+      marginLeft: 20,
+    },
+  });
 export default Task;
