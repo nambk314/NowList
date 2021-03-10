7457061f4903ef0161d185be669f132caf53d19c
Nam Bui <nambui@Nams-MacBook-Air.local>
2021-03-10 14:10:31 -0800

    Add Now.tsx

--- src/screens/Now.tsx
+++ src/screens/Now.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Task from '../components/Task';
 
 
 function Now() {
   return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Now</Text>
+        <View >
+            <Task task="Test Task"></Task>
         </View>
   );
 }
+
+const style = StyleSheet.create({
+    style: { 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+    }
+});
 
 export default Now;
